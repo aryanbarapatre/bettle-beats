bf6b09fbe28dfcf4047e8093e84dc17889b6a227
aryanbarapatre <128541350+aryanbarapatre@users.noreply.github.com>
2023-06-11 10:05:34 +0530

    bettle beats

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -30,12 +30,12 @@ const Modal: React.FC<ModalProps> = ({
              className="fixed drop-shadow-md border border-neutral-700 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-md bg-neutral-800 p-[25px] focus:outline-none overflow-auto"
             >
                 <Dialog.Title
-                    className="text-xl text-center font-bold mb-4"
+                    className="text-2xl text-center font-bold mb-4"
                 >
                     {title}
                 </Dialog.Title>
                 <Dialog.Description
-                    className="mb-5 text-sm leading-normal text-center"
+                    className="mb-5 text-md leading-normal text-center"
                 >
                     {description}
                 </Dialog.Description>
@@ -46,7 +46,7 @@ const Modal: React.FC<ModalProps> = ({
                     <button
                         className="text-neutral-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none"
                     >
-                        <IoMdClose />
+                        <IoMdClose size={25} />
                     </button>
                 </Dialog.Close>
             </Dialog.Content>
